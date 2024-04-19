f0e0414b273bc376c6055e92f9f8833b8342c434
tatiana <you@example.com>
2024-04-18 19:29:39 -0500

    Respuesta de preguntas del 1 al 5

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -2,3 +2,5 @@
 //'naranja'], utiliza el método asociado a los arrays para eliminar el primer
 //elemento, el último elemento e inserta al inicio del array el valor de nueva-fruta
 
+let frutas = ['manzana', 'plátano', 'pera','naranja']
+
